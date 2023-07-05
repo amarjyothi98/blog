da3b9b1b92bd9555958dad1345db109d985645bb
amarjyothi98 <amarjyothi526@gmail.com>
2023-07-05 15:10:38 +0530

    completed the home / blogpage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,61 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      blog hehe
-    </div>
+    <main>
+      <header className='firstHeader'>
+        <a href="heh" className="logo">Blog</a>
+        <nav>
+          <a href="heh">Login</a>
+          <a href="heh">Register</a>
+        </nav>
+      </header>
+      
+      <div className="post">
+        <div className="image">
+          <img src="https://images.unsplash.com/photo-1688320097436-a20f0b5ba1cc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1000&q=60" alt="" />
+        </div>
+        <div className="text">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a href="" className="author">Amar J</a>
+            <time>2023-07-05 14:51</time>
+          </p>
+          <p className='summary'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas vitae dolor illo in natus necessitatibus dolore. Eaque consequuntur, exercitationem numquam adipisci obcaecati molestiae laborum dolor et, ex nemo praesentium ea?</p>
+        </div>
+      </div>
+      
+      <div className="post">
+        <div className="image">
+          <img src="https://images.unsplash.com/photo-1688320097436-a20f0b5ba1cc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1000&q=60" alt="" />
+        </div>
+        <div className="text">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a href="" className="author">Amar J</a>
+            <time>2023-07-05 14:51</time>
+          </p>
+          <p className='summary'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas vitae dolor illo in natus necessitatibus dolore. Eaque consequuntur, exercitationem numquam adipisci obcaecati molestiae laborum dolor et, ex nemo praesentium ea?</p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://images.unsplash.com/photo-1688320097436-a20f0b5ba1cc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1000&q=60" alt="" />
+        </div>
+        <div className="text">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a href="" className="author">Amar J</a>
+            <time>2023-07-05 14:51</time>
+          </p>
+          <p className='summary'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas vitae dolor illo in natus necessitatibus dolore. Eaque consequuntur, exercitationem numquam adipisci obcaecati molestiae laborum dolor et, ex nemo praesentium ea?</p>
+        </div>
+      </div>
+
+     
+      
+     
+    </main>
   );
 }
 
